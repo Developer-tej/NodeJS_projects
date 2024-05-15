e128e72f9258992e72cbc267d3b4b7cc999f6391
Developer Tej <shivatej.developer@gmail.com>
2024-05-16 01:05:22 +0530

    Added a contact route

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ app.get('/',(req,res)=>{
     res.send("<h1>Hello on Home route</h1>");
 })
 
+app.get('/contact',(req,res)=>{
+    res.send("<h1>Ph-no : 9902083329</h1>");
+})
+
 app.listen(port,()=>{
     console.log(`Server running on port number ${port}`);
 })
