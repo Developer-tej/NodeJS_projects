b17e3247fa26e25afae135c234c952ec47250e29
Developer Tej <shivatej.developer@gmail.com>
2024-05-16 00:56:57 +0530

    Created a Home route

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
+app.get('/',(req,res)=>{
+    res.send("<h1>Hello on Home route</h1>");
+})
+
 app.listen(port,()=>{
     console.log(`Server running on port number ${port}`);
 })
